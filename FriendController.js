6ee4488d0f47c63d11d888c5caa5a10139af8abe
Jessica Hathaway <hathaway.jess@gmail.com>
2016-02-15 16:28:38 -0700

    Almost done

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,28 @@
+var app = angular.module('angularFriends');
 
 
+app.controller('FriendController', function($scope, $http) {
+  $scope.friends = [];
+	$http.get('friend-data.json')
+    .then(function(response) {
+      $scope.friends = response.data;
+    }, function(response) {
+      console.log('error');
+    });
 
+$scope.sort = [
+    {name: 'name', value: 'name'},
+    {name: 'friends', value: 'friend_count'},
+    {name: 'city', value: 'current_location.city'},
+    {name: 'state', value: 'current_location.state'},
+    {name: 'country', value: 'current_location.country'}
+  ];
 
-
+  $scope.ascDesc = [
+    {name: 'ascending', value: false},
+    {name: 'descending', value: true}
+  ];
+  $scope.sortType = 'name';
+  $scope.sorter = false;
+  
+  });
